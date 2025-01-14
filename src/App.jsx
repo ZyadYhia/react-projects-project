d4e7795d00f1767af204a291baee4851828e111e
ZyadYhia <zeyad.yhia95@gmail.com>
2025-01-14 13:49:30 +0200

    after init

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
+import ProjectsSidebar from "./Components/ProjectsSidebar";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    currentAction: "view",
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  let content;
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  console.log(projectsState);
+  
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAddProject={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  return (
+    <>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+        {content}
+      </main>
+    </>
+  );
+}
+
+export default App;
